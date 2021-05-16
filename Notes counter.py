990a6231012fa71e3b679c2a50f920e52fa6848d
Mathieu Michels <62720140+mathieumichels@users.noreply.github.com>
2021-05-16 18:02:19 +0200

    Commented version (a little too much ... oops).
    Now we have to go from the "per second" version to something more instantaneous. For example, which updates every 10th of a second. Or even ideally every time a note is played.
    
    But I haven't figured out how to do it yet. If anyone knows, I'm a taker;)

--- Notes counter.py
+++ Notes counter.py
@@ -1,35 +1,43 @@
 # ----------------------------------------------------------
 # import some packages
 # ----------------------------------------------------------
-
-from mido import MidiFile
-import moviepy.editor as mp
-from moviepy.editor import concatenate_videoclips
-import time
-from tkinter import *
-from tkinter.filedialog import *
+from mido import MidiFile # Open midi files
+import moviepy.editor as mp # Video editor
+from moviepy.editor import concatenate_videoclips # Merging videos
+import time # Because speed is important
+from tkinter import * # Visual interface
+from tkinter.filedialog import * # Easy file opening
 # ----------------------------------------------------------
 # init
 # ----------------------------------------------------------
 t = time.time()   # Because speed is important (lol)
-duration = 0      # Because I don't know why I initialise this variable anymore
+duration = 0      # Because I don't know why I initialize this variable anymore
 clips = []        # List of every video created with the video() function
 # ----------------------------------------------------------
-# midifile
+# Open midifile
 # ----------------------------------------------------------
 def midifile(filename):
     """
     :param filename: name of the .mid file to analyze.
     :return: a list of the number of notes per second: notes[i]= [second number #i, #j notes/sec]
     """
+    # --------------------------------------------------------------------
+    # Import some globals variables
+    # --------------------------------------------------------------------
+    global duration
+    # --------------------------------------------------------------------
+    # Generate a dictionary with the info from the midi file
+    # --------------------------------------------------------------------
     mid = MidiFile(filename)
     mididict = []
     for i in mid:
-        if i.type == 'note_on' or i.type == 'note_off' or i.type == 'time_signature':
+        if i.type == 'note_on':
             mididict.append(i.dict())
-    mem1=0
-    mem2=[]
-    mem3=[]
+    # --------------------------------------------------------------------
+    # Generate a list from the dictionary with only note_on and time info
+    # --------------------------------------------------------------------
+    mem1 = 0
+    mem2 = [] # Stores each time a note is played
     for i in mididict:
         time = i['time'] + mem1
         i['time'] = time
@@ -38,34 +46,32 @@ def midifile(filename):
             i['type'] = 'note_off'
         if i['type'] == 'note_on':
             mem2.append(i['time'])
-        if i['type'] == 'note_off':
-            mem3.append(i['time'])
 
-    #print("Number of notes:", len(mem2))
-    global duration
-    duration = max(mem2)
-    minutes = duration//60
-    secondes = duration - (minutes*60)
-    #print("Le morceau dure {} secondes, soit {} minutes et {} secondes.".format(duration,minutes,secondes))
-    #print("mem2",mem2)
-    for i in range(len(mem2)-1):
-        mem2[i] = int(mem2[i])
-    for i in range(len(mem3)-1):
-        mem3[i] = int(mem3[i])
-    print(len(mem2))
-    #print(len(mem1))
-    print(len(mem3))
-
-    notes = [[0,0]]
-
+    # --------------------------------------------------------------------
+    # Generate a list with the total amount of note per second
+    # --------------------------------------------------------------------
+    notes = [[0,0]] # Add one frame for second 0 and 0 note
     for i in range(int(duration)+1):
         notes.append([i+1,mem2.count(i)])
-    #print(notes)
-    notes.append([int(duration)+2,0])
-    print(notes)
-    print(len(notes))
+    notes.append([int(duration)+2,0]) # Add one frame for last second and 0 note
+
+    # --------------------------------------------------------------------
+    # Print some stupid stuff
+    # --------------------------------------------------------------------
+    duration = max(mem2)
+    minutes = duration // 60
+    secondes = duration - (minutes * 60)
+    print("Number of notes:", len(mem2), "\n\n")
+    print("The song has a duration of {} seconds, which is {} minutes and {} seconds..\n\n".format(duration, minutes,secondes))
+
+    # --------------------------------------------------------------------
+    # Return the list notes[i] = [time, notes/sec]
+    # --------------------------------------------------------------------
     return notes
-#midifile("Release/variation.mid")
+
+# ----------------------------------------------------------
+# Generate 1 frame
+# ----------------------------------------------------------
 def video(tps,txte,my_video,txte1,foldername):
     """
     :param tps: clip start time
@@ -76,7 +82,6 @@ def video(tps,txte,my_video,txte1,foldername):
     :return: A ".mp4" video with all of the above in the correct folder.
     Add the new video to the global list of video created.
     """
-
     # --------------------------------------------------------------------
     # video settings (size & time)
     # --------------------------------------------------------------------
@@ -107,7 +112,9 @@ def video(tps,txte,my_video,txte1,foldername):
     global clips
     clips.append(save_as)
 
-
+# ----------------------------------------------------------
+# Generate full video
+# ----------------------------------------------------------
 def video1(filename,foldername,videoname):
     """
     :param filename: name of the .mid file to analyze.
@@ -175,70 +182,119 @@ def video1(filename,foldername,videoname):
     advancement2.grid(column=1,row=9)
     advancement2.configure(text=finished,fg="green")
 
-
-
-
+# --------------------------------------------------------------------
+# Basic visual interface
+# --------------------------------------------------------------------
+# --------------------------------------------------------------------
+# Init Tkinter
+# --------------------------------------------------------------------
 window = Tk()
 window.title("Notes/secondes from midi files")
 window.geometry("700x250")
+# --------------------------------------------------------------------
+# 4 buttons, 4 fonctions
+# --------------------------------------------------------------------
 
-filename=""
 def clicked():
+    """
+    :return: global filename
+    """
     global filename
-    filename = askopenfilename(title="Choisissez votre fichier midi",
-                               filetypes=[('Midi file', '.mid')])
-    if len(filename)>0:
-        ouvert.configure(text="Fichier correctement ouvert",fg="green")
-        ouvert02.configure(text="Fichier "+str(filename)+" ouvert.")
-    return filename
-ouvert02 = Label(window,text="")
-ouvert02.grid(column=1,row=1)
-ouvert = Label(window,text="Aucun fichier ouvert",fg="red")
-ouvert.grid(column=0,row=0)
-ouvrirfichier = Button(window,text="Choisissez votre fichier midi",command=clicked)
-ouvrirfichier.grid(column=1,row=0)
-advancement = Label(window,text="0%",fg="red")
-advancement.grid(column=1,row=7)
-advancement1 = Label(window,text="0%",fg="red")
-advancement1.grid(column=1,row=8)
-videoname = ""
-def clicked1():
-    global videoname
-    videoname = askopenfilename(title="Choisissez une vidéo noire",filetypes=[('Video file', '.mp4')])
-    if len(videoname)>0:
-        ouvert1.configure(text="Fichier correctement ouvert",fg="green")
-        ouvert12.configure(text="Fichier " + str(videoname) + " ouvert.")
-    return videoname
-ouvert12 = Label(window,text="")
-ouvert12.grid(column=1,row=3)
-ouvert1 = Label(window,text="Aucun fichier ouvert",fg="red")
-ouvert1.grid(column=0,row=2)
-ouvrirfichier1 = Button(window,text="Choisissez une vidéo noire",command=clicked1)
-ouvrirfichier1.grid(column=1,row=2)
+    filename = askopenfilename(title="Choose your midi file",filetypes=[('Midi file', '.mid')])
 
-foldername = ""
+    if len(filename)>0: #checks if there is a file selected
+        open.configure(text="File successfully opened",fg="green")
+        open02.configure(text= str(filename)+" file opened.")
+
+    return filename
+
+def clicked1():
+    """
+    :return: global videoname
+    """
+    global videoname
+
+    videoname = askopenfilename(title="Choose the black video",filetypes=[('Video file', '.mp4')])
+
+    if len(videoname)>0: #checks if there is a file selected
+
+        open1.configure(text="Video successfully opened",fg="green")
+        open12.configure(text= str(videoname) + " video opened.")
+
+    return videoname
+
 def clicked2():
+    """
+    :return: global foldername
+    """
     global foldername
-    foldername = askdirectory(title="Choisissez un dossier de sortie")
-    if len(foldername)>0:
-        ouvert2.configure(text="Dossier correctement ouvert",fg="green")
-        ouvert22.configure(text="Dossier "+str(foldername)+" ouvert.")
+    foldername = askdirectory(title="Choose an output folder")
+
+    if len(foldername)>0: #checks if there is a folder selected
+        open2.configure(text="Folder successfully opened",fg="green")
+        open22.configure(text= str(foldername) +" folder opened.")
+
     return foldername
-ouvert22 = Label(window,text="")
-ouvert22.grid(column=1,row=5)
-ouvert2 = Label(window,text="Aucun fichier ouvert",fg="red")
-ouvert2.grid(column=0,row=4)
-ouvrirfichier2 = Button(window,text="Choisissez un dossier de sortie",command=clicked2)
-ouvrirfichier2.grid(column=1,row=4)
 
 def clicked3():
+    """
+    :return: successfully created video
+    """
     global foldername,filename,videoname,t
+    # Because speed is important (I know, I don't return or print time, but it's OK.)
     t = time.time()
     video1(filename,foldername,videoname)
     return foldername
 
-
-ouvrirfichier3 = Button(window,text="Rendre la vidéo",command=clicked3)
-ouvrirfichier3.grid(column=1,row=6)
-
+# --------------------------------------------------------------------
+# Initialize global filename, videoname & foldername
+# --------------------------------------------------------------------
+filename   = ""
+videoname  = ""
+foldername = ""
+# --------------------------------------------------------------------
+# Configure texts and buttons of the Tkinter window
+# --------------------------------------------------------------------
+# --------------------------------------------------------------------
+# Midi file
+# --------------------------------------------------------------------
+open02 = Label(window,text="")
+open02.grid(column=1,row=1)
+open = Label(window,text="No files opened",fg="red")
+open.grid(column=0,row=0)
+openfile = Button(window,text="Choose your midi file",command=clicked)
+openfile.grid(column=1,row=0)
+# --------------------------------------------------------------------
+# Black video
+# --------------------------------------------------------------------
+open12 = Label(window,text="")
+open12.grid(column=1,row=3)
+open1 = Label(window,text="No files opened",fg="red")
+open1.grid(column=0,row=2)
+openfile1 = Button(window,text="Choose the black video",command=clicked1)
+openfile1.grid(column=1,row=2)
+# --------------------------------------------------------------------
+# Output folder
+# --------------------------------------------------------------------
+open22 = Label(window,text="")
+open22.grid(column=1,row=5)
+open2 = Label(window,text="No files opened",fg="red")
+open2.grid(column=0,row=4)
+openfile2 = Button(window,text="Choose an output folder",command=clicked2)
+openfile2.grid(column=1,row=4)
+# --------------------------------------------------------------------
+# Render the video
+# --------------------------------------------------------------------
+openfile3 = Button(window,text="Render the video",command=clicked3)
+openfile3.grid(column=1,row=6)
+# --------------------------------------------------------------------
+# ... progress bar ...
+# --------------------------------------------------------------------
+advancement = Label(window,text="0%",fg="red")
+advancement.grid(column=1,row=7)
+advancement1 = Label(window,text="0%",fg="red")
+advancement1.grid(column=1,row=8)
+# --------------------------------------------------------------------
+# And of course...
+# --------------------------------------------------------------------
 window.mainloop()
